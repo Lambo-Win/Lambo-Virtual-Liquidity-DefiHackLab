0ab83e46d8580955ea74527cb5cfb3a6784af6bf
shaneson <shaneson@shanesondeMacBook-Pro.local>
2024-12-18 14:30:23 +0800

    deloy VETH

--- script/0.deployTokens.s.sol
+++ script/0.deployTokens.s.sol
@@ -9,13 +9,13 @@ import {LaunchPadUtils} from "../src/Utils/LaunchPadUtils.sol";
 import "forge-std/console2.sol";
 
 contract DeployTokens is Script {
-    // forge script script/0.deployTokens.s.sol:DeployTokens --rpc-url wss://ethereum-rpc.publicnode.com --broadcast -vvvv --legacy --verify --etherscan-api-key RTJCFXFDI87EIYGXH8BT1RJZRQ6IY85N3Q
+    // forge script script/0.deployTokens.s.sol:DeployTokens --rpc-url https://eth.llamarpc.com --broadcast -vvvv --legacy --verify --etherscan-api-key RTJCFXFDI87EIYGXH8BT1RJZRQ6IY85N3Q
     function run() external {
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(privateKey);
         LamboToken lamboTokenV2 = new LamboToken();
-        VirtualToken vETH = new VirtualToken("vETH", "vETH", LaunchPadUtils.NATIVE_TOKEN);
+        VirtualToken vETH = new VirtualToken("VETH", "VETH", LaunchPadUtils.NATIVE_TOKEN);
         vm.stopBroadcast();
 
         console2.log("LamboToken address:", address(lamboTokenV2));
--- src/VirtualToken.sol
+++ src/VirtualToken.sol
@@ -1,3 +1,32 @@
+/*
+                    Ohf                              thm       
+                    Ohf                              thm       
+                    !LLbhf                              thkLLi    
+                    <hhhhf                              thhhh~    
+                    ,((qhLf/                          /fLhp((,    
+                    \xLhb{{:                    ,{{dhLx/       
+                        |hhhh1IIIIIIIIIIIIIIIIIIII[qqqq)         
+                    .,,,,thhbbJvvvvxrrrrrrrrrrrrrrrnvvvv[,,,,.    
+                    <hhhhhhkvvvvvvv'               lvvvv0hhhh~    
+                    <hhhhhhkvvvvt<<                 ''xv0hhhh~    
+                (Jv<<<<nhkvvvvtii  1UUUU;   .UUUUf  rv(<<<<uJ|  
+                jj\(]    |hkvvvvtii::nhhhh!   'hhhhY  rv-    ?(\jj
+                cc~      |hkvvvvj[[>i/UUUUi^^^"UUUUxiinv-      <cc
+                    ,:fhkvvvvvvv_~~~~~~~~~~~~~~~[vvvv]^^^^.    
+                    OhhhkvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvI    
+                fwwwwkhhhkvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvxr[  
+                vvZhhhhhhwCCvvvvvvvvvj|(]]]]]]]]]]]]]]]]]]]]]rvj11
+                hhhhhhhhhOvvvvvvvvvvv\[{\\\\[lllllllll(\\\(lltvvvv
+                hhhhhhhhhOvvvvvvvvvvv\[\hhhhclllllllllqhhhwlltvvvv
+                hhhhhhhhhOvvvvvvvvvvv\[\hhhhclllllllllqhhhwlltvvvv
+                hhhhhhhhhOvvvvvvvvvvv\[}||]-~lllllllll_---_lltvCdd
+                hhhhhhhhhpO0vvvvvvvvvrt/[[_++++llllllllllli))xvLhh
+                hhhhhhhhhhhkUUUUYvvvvvvn))}[[[[>>>>>>>>>~+]UUUUOhh
+                hhhhhhhhhhhhhhhhwvvvvvvvvv)[[[[[[[[[[[[[tvXhhhhhhh
+                """""""""/hhhhhhhbbbbbbbbbddddddddddddddbbkhh-""""
+                        !11111111111Jhhhhhhhhhhhhhhhhhhhhhhh~    
+*/
+
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.20;
 
